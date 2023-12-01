7ebe9bae259f7aec886c67f1f295d35fd56c877a
oliver <caleb19291999@outlook.com>
2023-12-01 07:20:53 -0800

    updated code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/olgrt"
+                href="https://github.com/lwblack"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
